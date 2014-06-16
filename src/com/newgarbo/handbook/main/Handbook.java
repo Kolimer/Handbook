9948e7b95e27853170c04dbd89f034ca04a7dbb3
NewGarbo <benkiveris@hotmail.com>
2014-06-16 18:31:47 +0100

    Added default methods to main plugin class

--- src/com/newgarbo/handbook/main/Handbook.java
+++ src/com/newgarbo/handbook/main/Handbook.java
@@ -2,6 +2,23 @@ package com.newgarbo.handbook.main;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Handbook extends JavaPlugin {
-
+public class Handbook extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+	}
+	
+	@Override
+	public String toString()
+	{
+		return this.getDescription().getFullName();
+	}
+	
 }
