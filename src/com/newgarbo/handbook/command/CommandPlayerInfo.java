de8c765f5f227de04a55df28708211a8ef50bb55
NewGarbo <benkiveris@hotmail.com>
2014-06-17 12:55:32 +0100

    Added some nicer formatting to CommandPlayerInfo

--- src/com/newgarbo/handbook/command/CommandPlayerInfo.java
+++ src/com/newgarbo/handbook/command/CommandPlayerInfo.java
@@ -26,13 +26,13 @@ public class CommandPlayerInfo extends Command
 			{
 				Player t = Bukkit.getPlayer(args[0]);
 				
-				sender.sendMessage(ChatColor.BLUE + "-=-=-=Displaying player info for " + ChatColor.GREEN + t.getName() + ChatColor.BLUE + "=-=-=-");
-				sender.sendMessage(ChatColor.BLUE + "UUID: " + t.getUniqueId().toString());
-				sender.sendMessage(ChatColor.BLUE + "Display name: " + t.getDisplayName());
-				sender.sendMessage(ChatColor.BLUE + "Hunger: " + t.getFoodLevel());
-				sender.sendMessage(ChatColor.BLUE + "Health: " + t.getHealth());
-				sender.sendMessage(ChatColor.BLUE + "X: " + t.getLocation().getBlockX() + ", Y: " + t.getLocation().getBlockY() + ", Z:" + t.getLocation().getBlockZ());
-				sender.sendMessage(ChatColor.BLUE + "Pitch: " + t.getLocation().getPitch() + ", Yaw: " + t.getLocation().getYaw());
+				sender.sendMessage(ChatColor.GOLD + "-=-=-=Displaying player info for " + ChatColor.GREEN + t.getName() + ChatColor.GOLD + "=-=-=-");
+				sender.sendMessage(ChatColor.GOLD + "UUID: " + ChatColor.RED + t.getUniqueId().toString());
+				sender.sendMessage(ChatColor.GOLD + "Display name: " + ChatColor.RED + t.getDisplayName());
+				sender.sendMessage(ChatColor.GOLD + "Hunger: " + ChatColor.RED + t.getFoodLevel());
+				sender.sendMessage(ChatColor.GOLD + "Health: " + ChatColor.RED + t.getHealth());
+				sender.sendMessage(ChatColor.GOLD + "X: " + ChatColor.RED + t.getLocation().getBlockX() + ChatColor.GOLD + ", Y: " + ChatColor.RED + t.getLocation().getBlockY() + ChatColor.GOLD + ", Z: " + ChatColor.RED + t.getLocation().getBlockZ());
+				sender.sendMessage(ChatColor.GOLD + "Pitch: " + ChatColor.RED + t.getLocation().getPitch() + ChatColor.GOLD + ", Yaw: " + ChatColor.RED + t.getLocation().getYaw());
 			}
 			else
 			{
